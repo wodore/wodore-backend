3b9a04ed6a4e907c9d992ce4386f7e0a82a4315f
Tobias Burgherr <tb@wodore.com>
2023-12-30 14:03:57 +0100

    added trusted originas for https

--- server/settings/environments/development.py
+++ server/settings/environments/development.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "[::1]",
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    f"http://{config('DOMAIN_NAME')}",
+    f"https://{config('DOMAIN_NAME')}",
+]
 # CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
