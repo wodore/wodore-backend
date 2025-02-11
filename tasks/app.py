cc665aaf7afbde98a5df56f21b22dc327210bf4c
Tobias Burgherr <tburgherr@gmail.com>
2025-02-11 15:48:56 +0100

    Add app command

--- tasks/app.py
+++ tasks/app.py
@@ -16,6 +16,21 @@ from tasks import (  # noqa: F401
     help={
         "infisical": "Use infisical to run it (secret manager)",
     },
+)
+def app(c: Ctx, infisical: bool = False, cmd: str = ""):
+    """Run any app command with --cmd. E.g. 'inv app.app -i --cmd migrate'"""
+    cmd_ = ""
+    if infisical:
+        cmd_ += "infisical run --env=dev --path /backend --silent --log-level warn -- "
+    cmd_ += f"app {cmd}"
+    info(f"Run '{cmd_}'")
+    c.run(cmd_)
+
+
+@task(
+    help={
+        "infisical": "Use infisical to run it (secret manager)",
+    },
     aliases=["runserver"],
 )
 def run(c: Ctx, port: str = "8093", infisical: bool = False):
