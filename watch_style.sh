91baa74bcda2b402edb1ad8f0f5c72492f67f17d
Tobias Burgherr <tb@wodore.com>
2025-07-25 23:42:33 +0200

    Update python packages (#13)

--- watch_style.sh
+++ watch_style.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx tailwindcss -o server/apps/admin/static/css/styles.css --watch --minify
+npx tailwindcss -i styles.css -o server/apps/manager/static/css/styles.css --watch --minify
