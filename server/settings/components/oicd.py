9241e4be7cae6cf61d30984ecd2565300fdde78c
Tobias Burgherr <tb@wodore.com>
2026-01-01 16:19:39 +0100

    Fix oidc issues (#39)

--- server/settings/components/oicd.py
+++ server/settings/components/oicd.py
@@ -121,8 +121,11 @@ if discovery_info:
         if config("DJANGO_ADMIN_URL", None)
         else "http://localhost:8000"
     )
+    # Redirect to /admin after successful login
     LOGIN_REDIRECT_URL = f"{_django_admin_url}/admin"
-    LOGOUT_REDIRECT_URL = f"{_django_admin_url}/admin"
+    LOGOUT_REDIRECT_URL = (
+        f"{_django_admin_url}/"  # Does not work, still goes to login page again
+    )
     LOGIN_URL = f"{_django_admin_url}/oidc/authenticate/"
 
     ZITADEL_API_MACHINE_USERS = {
